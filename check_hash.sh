a433dae29b5ff91e9022ff1264dc5c12e80da0ba
theernis <ernestas.pakstas@gmail.com>
2024-06-01 21:44:43 +0300

    script is kinda working rn (only few cases work)

--- check_hash.sh
+++ check_hash.sh
@@ -3,8 +3,6 @@
 HASH_INFO=$1
 PASSWORD=$2
 
-echo $HASH_INFO
-
 if [[ $HASH_INFO =~ \$(.*) ]] ; then
 	HASH_TYPE=$(awk -F$ '{ print($2) }' <<< $HASH_INFO)
 else
@@ -12,7 +10,32 @@ else
 	exit 1
 fi
 
+hash_openssl() {
+	SALT=$(awk -F$ '{ print($3) }' <<< $HASH_INFO)
+	case $HASH_TYPE in 
+		1)
+			HASH=$(openssl passwd -1 -salt "$SALT" "$PASSWORD")
+		;;
+		5)
+			HASH=$(openssl passwd -5 -salt "$SALT" "$PASSWORD")
+		;;
+		6)
+			HASH=$(openssl passwd -6 -salt "$SALT" "$PASSWORD")
+		;;
+		*)
+			exit 1
+		;;
+	esac
+	if [[ $HASH_INFO = $HASH ]] then
+		exit 0
+	fi
+	exit 1
+}
+
 case $HASH_TYPE in
+	1 | 5 | 6)
+		hash_openssl
+	;;
 	*)
 		echo "unknown hash type"
 		exit 1
